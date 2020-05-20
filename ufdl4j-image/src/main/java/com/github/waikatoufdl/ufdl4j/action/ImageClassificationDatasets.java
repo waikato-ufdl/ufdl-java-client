7ebb9bd26409f5be8d111de08eabf517d2e8016e
Peter Reutemann <fracpete@gmail.com>
2020-05-20 15:42:41 +1200

    added ImageClassificationDataset class with access to files

--- ufdl4j-image/src/main/java/com/github/waikatoufdl/ufdl4j/action/ImageClassificationDatasets.java
+++ ufdl4j-image/src/main/java/com/github/waikatoufdl/ufdl4j/action/ImageClassificationDatasets.java
@@ -5,6 +5,12 @@
 
 package com.github.waikatoufdl.ufdl4j.action;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Encapsulates dataset operations for image classification.
  *
@@ -14,6 +20,41 @@ public class ImageClassificationDatasets
   extends Datasets {
 
   private static final long serialVersionUID = -6031589074773486947L;
+
+  /**
+   * Container class for image classification dataset information.
+   */
+  public static class ImageClassificationDataset
+    extends Dataset {
+
+    /**
+     * Initializes the dataset.
+     *
+     * @param data the data to use
+     */
+    public ImageClassificationDataset(JsonObject data) {
+      super(data);
+    }
+
+    /**
+     * Returns the files of this dataset.
+     *
+     * @return		the files
+     */
+    public List<String> files() {
+      List<String> result;
+      JsonArray array;
+      int i;
+
+      result = new ArrayList<>();
+      if (getData().has("files")) {
+	array = getData().getAsJsonArray("files");
+	for (i = 0; i < array.size(); i++)
+	  result.add(array.get(i).getAsString());
+      }
+      return result;
+    }
+  }
 
   /**
    * The URL path to use.
