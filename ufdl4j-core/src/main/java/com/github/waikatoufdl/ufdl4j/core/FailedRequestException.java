8327e344364c4fcf4771f1d74f2129fd282fc530
Peter Reutemann <fracpete@gmail.com>
2020-08-29 17:53:46 +1200

    improved text of exception, including body if available

--- ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/core/FailedRequestException.java
+++ ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/core/FailedRequestException.java
@@ -1,10 +1,11 @@
 /*
  * FailedRequestException.java
- * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ * Copyright (C) 2019-2020 University of Waikato, Hamilton, NZ
  */
 
 package com.github.waikatoufdl.ufdl4j.core;
 
+import com.github.fracpete.requests4j.response.BasicResponse;
 import com.github.fracpete.requests4j.response.Response;
 
 /**
@@ -24,6 +25,32 @@ public class FailedRequestException
    * @param response	the response of the failed request
    */
   public FailedRequestException(String msg, Response response) {
-    super(response.statusCode() + "/" + response.statusMessage() + ": " + msg);
+    super(toString(msg, response));
+  }
+
+  /**
+   * Turns the response into a string.
+   *
+   * @param response 	the response to turn into a string.
+   * @return		the generated string
+   */
+  protected static String toString(String msg, Response response) {
+    StringBuilder	result;
+    String		body;
+
+    result = new StringBuilder(msg);
+    result.append("\n").append(response.statusCode()).append(": ").append(response.statusMessage());
+    if (response instanceof BasicResponse) {
+      try {
+        body = ((BasicResponse) response).text();
+        if (!body.isEmpty())
+          result.append("\n").append(body);
+      }
+      catch (Exception e) {
+        // ignored
+      }
+    }
+
+    return result.toString();
   }
 }
