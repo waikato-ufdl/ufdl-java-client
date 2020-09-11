159250dde5f6c43184168bdcd3085043cbb04455
Peter Reutemann <fracpete@gmail.com>
2020-09-11 14:52:10 +1200

    tokens are now stored per server URL

--- ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/auth/MemoryOnlyStorage.java
+++ ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/auth/MemoryOnlyStorage.java
@@ -5,16 +5,24 @@
 
 package com.github.waikatoufdl.ufdl4j.auth;
 
+import com.github.waikatoufdl.ufdl4j.core.AbstractLoggingObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Stores the tokens only in memory.
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  */
 public class MemoryOnlyStorage
+  extends AbstractLoggingObject
   implements TokenStorageHandler {
 
+  private static final long serialVersionUID = -7096896606724147641L;
+
   /** the tokens. */
-  protected Tokens m_Tokens;
+  protected Map<String,Tokens> m_Tokens;
 
   /**
    * Retrieves the tokens for the user.
@@ -24,9 +32,19 @@ public class MemoryOnlyStorage
    */
   @Override
   public synchronized Tokens load(Authentication context) {
+    String	url;
+
+    url = context.getServer().getURL();
+
     if (m_Tokens == null)
-      m_Tokens = new Tokens();
-    return m_Tokens;
+      m_Tokens = new HashMap<>();
+
+    if (!m_Tokens.containsKey(url)) {
+      getLogger().info("Creating empty tokens for: " + url);
+      m_Tokens.put(url, new Tokens());
+    }
+
+    return m_Tokens.get(url);
   }
 
   /**
@@ -38,7 +56,26 @@ public class MemoryOnlyStorage
    */
   @Override
   public synchronized String store(Authentication context, Tokens tokens) {
-    m_Tokens = tokens;
+    String	url;
+
+    url = context.getServer().getURL();
+
+    if (m_Tokens == null)
+      m_Tokens = new HashMap<>();
+
+    getLogger().info("Storing tokens for: " + url);
+    m_Tokens.put(url, tokens);
+
     return null;
+  }
+
+  /**
+   * Returns a short description of the state.
+   *
+   * @return		the description
+   */
+  @Override
+  public String toString() {
+    return m_Tokens.toString();
   }
 }
