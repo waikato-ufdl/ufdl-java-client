4f71efa2343ab021859887089621b09fde45a5ce
Peter Reutemann <fracpete@gmail.com>
2023-04-28 21:33:48 +1200

    added method to turn map into JsonObject

--- ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/core/JsonUtils.java
+++ ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/core/JsonUtils.java
@@ -1,6 +1,6 @@
 /*
  * JsonUtils.java
- * Copyright (C) 2020-2021 University of Waikato, Hamilton, NZ
+ * Copyright (C) 2020-2023 University of Waikato, Hamilton, NZ
  */
 
 package com.github.waikatoufdl.ufdl4j.core;
@@ -14,6 +14,7 @@ import com.google.gson.JsonPrimitive;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Helper class for JSON related tasks.
@@ -103,15 +104,43 @@ public class JsonUtils {
     result = new JsonArray();
     for (Object value: values) {
       if (value instanceof String)
-        result.add((String) value);
+	result.add((String) value);
       else if (value instanceof Number)
-        result.add((Number) value);
+	result.add((Number) value);
       else if (value instanceof Boolean)
-        result.add((Boolean) value);
+	result.add((Boolean) value);
       else if (value instanceof Character)
-        result.add((Character) value);
+	result.add((Character) value);
       else
-        throw new IllegalStateException("Unhandled list element type: " + value.getClass().getName());
+	throw new IllegalStateException("Unhandled list element type: " + value.getClass().getName());
+    }
+
+    return result;
+  }
+
+  /**
+   * Turns a map into a json object.
+   *
+   * @param map		the map to convert
+   * @return		the generated json object
+   */
+  public static JsonObject mapToJson(Map map) {
+    JsonObject  result;
+    Object	obj;
+    String	key;
+
+      result = new JsonObject();
+    for (Object keyObj: map.keySet()) {
+      key = "" + keyObj;
+      obj = map.get(keyObj);
+      if (obj instanceof Number)
+        result.addProperty(key, (Number) obj);
+      else if (obj instanceof Boolean)
+	result.addProperty(key, (Boolean) obj);
+      else if (obj instanceof String)
+	result.addProperty(key, (String) obj);
+      else if (obj instanceof Map)
+	result.add(key, mapToJson((Map) obj));
     }
 
     return result;
