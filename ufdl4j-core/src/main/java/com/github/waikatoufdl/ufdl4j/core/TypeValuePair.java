b1665103c943ce09e1c818a8ca3a224def718999
Peter Reutemann <fracpete@gmail.com>
2023-04-28 21:52:53 +1200

    helper class for generating type-value pair map

---
+++ ufdl4j-core/src/main/java/com/github/waikatoufdl/ufdl4j/core/TypeValuePair.java
@@ -0,0 +1,34 @@
+/*
+ * TypeValueMap.java
+ * Copyright (C) 2023 University of Waikato, Hamilton, New Zealand
+ */
+
+package com.github.waikatoufdl.ufdl4j.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Returns a map of a type-value pair.
+ *
+ * @author fracpete (fracpete at waikato dot ac dot nz)
+ */
+public class TypeValuePair {
+
+  /**
+   * Generates a map from a type-value pair.
+   *
+   * @param type	the type
+   * @param value	the value
+   * @return		the generated map
+   */
+  public static Map<String,String> typeValuePair(String type, String value) {
+    Map<String,String>  result;
+
+    result = new HashMap<>();
+    result.put("type", type);
+    result.put("value", value);
+
+    return result;
+  }
+}
